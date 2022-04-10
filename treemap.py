a3bda6915f9b17ac66a57b4a4f2dce207d8c6de9
hanmingli28 <hmli_alex@hotmail.com>
2022-04-10 11:44:44 -0400

    midphase

--- treemap.py
+++ treemap.py
@@ -3,6 +3,7 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 import re
+pd.set_option("display.max_rows", None)
 
 # Load data
 dat_exp16 = pd.read_csv("./data/USA_ALL_export_2016_allproduct.csv")
@@ -54,8 +55,7 @@ dat_exp16_world = dat_exp16_lit.loc[dat_exp16_lit['partner'] == 'World']
 dat_exp16_world.drop(dat_exp16_world.tail(1).index,inplace=True)
 # Convert object to int
 dat_exp16_world["commodity_code"] = dat_exp16_world.commodity_code.astype(np.int64)
-dat_exp16_world["qty"] = dat_exp16_world.qty.astype(np.int64)
-# print(dat_exp16_world.dtypes)
+dat_exp16_world["qty"] = dat_exp16_world.qty.astype(np.float64)
 
 # Compute the sum of commodity quantity for all root categories
 dat_exp16_w_temp = dat_exp16_world.loc[dat_exp16_world['commodity_code'] > 10000]
@@ -70,23 +70,42 @@ qty_sum = qty_sum.reset_index()
 dat_exp16_w  = dat_exp16_world.loc[dat_exp16_world['commodity_code'] < 100]
 dat_exp16_w = dat_exp16_w.reset_index()
 dat_exp16_w.drop(["qty"],axis=1,inplace=True)
+
+# Drop all rows with either 0 qty or trade value
 dat_exp16_w["qty"]=qty_sum["qty"]
-# dat_exp16_w=pd.concat([dat_exp16_w, qty_sum['qty']], axis=1, ignore_index=True)
-print(dat_exp16_w.tail())
+dat_exp16_w = dat_exp16_w[(dat_exp16_w[['trade_value__us__','qty']] != 0).all(axis=1)]
+
+# Rescale column quantity
+dat_exp16_w["qty"]=np.log(dat_exp16_w["qty"])
+
 # print(len(qty_sum), len(dat_exp16_w))
 
 
+# print(dat_exp16_w.tail())
+# print(dat_exp16_w.dtypes)
+
+# Sort
+dat_exp16_w.sort_values(by=['trade_value__us__'], ascending=False, inplace=True)
+dat_exp16_w=dat_exp16_w.reset_index()
+print(dat_exp16_w)
+
 # exit()
 
-df = dat_exp16_w
-df["world"] = "world"  # in order to have a single root node
+# Plot the treemap
+# df = dat_exp16_w
+dat_exp16_w["world"] = "Commodity Categories"  # in order to have a single root node
 fig = px.treemap(
-    df,
+    dat_exp16_w,
     path=["world", "commodity"],  # << sets hierarchy
-    values="qty",
-    color="trade_value__us__",
-    hover_data=["trade_value__us__"],
+    values="trade_value__us__",
+    color="qty",
+    # hover_data=["year"],
     color_continuous_scale="RdBu",
-    color_continuous_midpoint=np.average(df["trade_value__us__"], weights=df["qty"]),
+    color_continuous_midpoint=np.average(dat_exp16_w["qty"]),
+    labels={"qty":"Log(quantity"},
 )
+year = dat_exp16_w.year.tolist()
+quantity = dat_exp16_w.qty.tolist()
+fig.data[0].customdata = np.column_stack([year, quantity])
+fig.data[0].hovertemplate = 'Category = %{label}<br>Year = %{customdata[0]}<br>Trade value(US$) = %{value}<br>Quantity = %{customdata[1]}'
 fig.show()
